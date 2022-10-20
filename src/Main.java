b2c83748bea017a7f6b0e1288e034ec392e2dbf3
Qamar Yochanan <qamar.yochanan@gmail.com>
2022-10-19 20:46:30 -0500

    Secondary Commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 /* todo: import scanner, import array
 
+todo 2: Start with, While !Q - map out ops
+ 2) Comit
+
  */
 
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    static Shop tarjay1 = new Shop("New Brighton", 508);
+    static Shop targay = new Shop("New Brighton", 508);
     static Scanner jts = new Scanner(System.in);
 
     static void ucryOptionsM() {
@@ -22,8 +25,64 @@ public class Main {
     }
 
     public static void main(String[] args) {
-     tarjay1.addProduct("hair stuff",13502,9.99,true);
-     tarjay1.printInventory();
+    ucryOptionsM();
+    //Manager password option
+    String reply = jts.nextLine();
+
+    while (!reply.equalsIgnoreCase("Q")) {
+
+        //Create
+        if(reply.equalsIgnoreCase("C") ) {
+
+            //Asks product ID
+            System.out.println("Please Enter Product ID: ");
+            /* add conditions--is only numbers
+             */
+            int pID = jts.nextInt();
+            jts.nextLine();
+
+            //asks product Name
+            System.out.println("Please Enter Product Name");
+            String pName = jts.nextLine();
+
+            //Asks product cost
+            System.out.println("Please Enter Product Cost ");
+            double pCost = jts.nextDouble();
+            jts.nextLine();
+
+            //Asks in stock
+            System.out.println("Is Product in Stock?");
+            boolean inStock = jts.nextBoolean();
+            jts.nextLine();
+
+            //creates new product
+            Product newProduct = new Product(pName, pID, pCost, inStock);
+            targay.products.add(newProduct);
+            targay.printInventory();
+            //asks same Question again until user inputs Q
+            ucryOptionsM();
+            reply = jts.nextLine();
+
+            //Read
+        } else if (reply.equalsIgnoreCase("R")) {
+            targay.printInventory();
+            System.out.println("What do you want to do next?");
+            ucryOptionsM();
+            reply = jts.nextLine();
+
+            //Update
+        } else if (reply.equalsIgnoreCase("U")) {
+            System.out.println("Please enter product ID Number");
+            int productID = jts.nextInt();
+            jts.nextLine();
+            Product updatedP = targay.findProductByPID(productID);
+
+        }
+
+        //Update
+    }
+
+
 
 
 
